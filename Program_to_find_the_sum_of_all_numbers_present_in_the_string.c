4309d735d789169d2cf16082e952b05a2fd6aa33
21MH1A04K2 <98155484+21MH1A04K2@users.noreply.github.com>
2022-05-17 19:59:04 +0530

    #include<stdio.h> #include<string.h> int main() { char str[100]; scanf("%s",str); int sum; for(int i=0;i<strlen(str);i++) { if(str[i]>=48&&str[i]<=57) { sum=sum+str[i]-48; } }printf("%d",sum); }

---
+++ Program_to_find_the_sum_of_all_numbers_present_in_the_string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[100];
+scanf("%s",str);
+int sum;
+for(int i=0;i<strlen(str);i++)
+{
+if(str[i]>=48&&str[i]<=57)
+{
+sum=sum+str[i]-48;
+}
+}printf("%d",sum);
+}
